a91c39e1e72dc7b37a32a50e86520ecebb6bfba1
Arif Kurdi <sasuarif90@gmail.com>
2026-06-15 17:44:15 +0200

    Refactor TwoSum method for consistency

--- Data Structures & Algorithms/two-integer-sum/submission-0.cs
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cs
@@ -1,15 +1,15 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        Dictionary<int, int> numMap =
-            new Dictionary<int, int>();  // A mapping to store numbers and their indices
+        Dictionary<int, int> numMap = new Dictionary<int, int>(); 
         for (int i = 0; i < nums.Length; i++) {
-            int complement = target - nums[i];  // Find the required number to reach the target
+
+            int complement = target - nums[i];  
+            
             if (numMap.ContainsKey(complement)) {
-                return new int[] { numMap[complement],
-                                   i };  // Return indices of the complement and current number
+                return new int[] { numMap[complement], i };
             }
-            numMap[nums[i]] = i;  // Store the number with its index
+            numMap[nums[i]] = i;
         }
-        return new int[] {};  // This line is never reached due to the problem guarantee
+        return new int[] {}; 
     }
 }
